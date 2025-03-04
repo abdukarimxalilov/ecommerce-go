fa4b0f4dbe36c8677ea4f9913850bf254c65fac1
Abdukarim <xalilovabdikarim26@gmail.com>
2025-03-05 00:20:47 +0500

    bug fixed

--- controller/cart.go
+++ controller/cart.go
@@ -15,12 +15,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-
 	type Application struct {
 		prodCollection *mongo.Collection
 		userCollection *mongo.Collection
 	}
-
 	func NewApplication(prodCollection, userCollection *mongo.Collection) *Application{
 		return &Application{
 			prodCollection: prodCollection,
